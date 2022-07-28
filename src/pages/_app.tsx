9283fb5116697cbb8909710cb9d2f8defb9ab545
joeelmahallawy <youssef.elmahallawy01@gmail.com>
2022-07-28 15:21:16 -0700

    updated ga tracking code

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { AppProps } from "next/app";
-
+import { DefaultSeo, NextSeoProps } from "next-seo";
+import logo from "../../assets/chartify.png";
 import Head from "next/head";
 import { MantineProvider } from "@mantine/core";
 import React from "react";
@@ -7,6 +8,7 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { sizes } from "../utils";
 import { NotificationsProvider } from "@mantine/notifications";
 import icon from "../../assets/chartify.png";
+
 import { GoogleAnalytics, usePageViews } from "nextjs-google-analytics";
 
 export default function App(props: AppProps) {
@@ -34,13 +36,83 @@ export default function App(props: AppProps) {
           },
         }}
       >
-        <GoogleAnalytics gaMeasurementId="G-50M1PPDEQK" />
+        <GoogleAnalytics
+          gaMeasurementId={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}
+        />
         <NotificationsProvider position="bottom-center" zIndex={99999999}>
           <ChakraProvider>
+            <DefaultSeo {...createSEOConfig()} />
             <Component {...pageProps} />
           </ChakraProvider>
         </NotificationsProvider>
       </MantineProvider>
     </>
   );
+}
+
+const config = {
+  author: "Youssef El Mahallawy",
+  siteName: "Chartify",
+  siteDescription: "Create charts with a single configurable API.",
+  defaultPageTitle: "Chartify",
+  blogTitle: "Chart API",
+  baseUrl: "www.chartify.dev",
+  websiteLogo: logo.src,
+};
+
+type DataType = {
+  title?: string;
+  seoDescription?: string;
+  canonicalUrl?: string;
+  imageUrl?: string;
+  slug?: string;
+  publishDate?: string;
+  modifiedDate?: string;
+};
+
+const getImage = (data: DataType = {}) => {
+  if (data.imageUrl) {
+    return [{ url: data.imageUrl, width: 600, height: 300, alt: data.title }];
+  }
+
+  if (data.slug) {
+    return [
+      {
+        url: `/${data.slug}`,
+        width: 600,
+        height: 300,
+        alt: data.title,
+      },
+    ];
+  }
+
+  return [
+    {
+      url: config.websiteLogo,
+      width: 280,
+      height: 280,
+      alt: "Chart API",
+    },
+  ];
+};
+
+export function createSEOConfig(data: DataType = {}): NextSeoProps {
+  const title = data.title || config.defaultPageTitle;
+  const description = data.seoDescription
+    ? data.seoDescription
+    : config.siteDescription;
+
+  return {
+    title,
+    description,
+    openGraph: {
+      type: "website",
+      locale: "en_US",
+      url: data.canonicalUrl,
+      title,
+      description,
+      images: getImage(data),
+      site_name: config.siteName,
+    },
+  };
 }
